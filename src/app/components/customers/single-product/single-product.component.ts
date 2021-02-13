5d23c5c3c72a8a41fb114e74c7b969f8a44ce6ea
Abdelrahman-Elshimy <abdoelsimy123@gmail.com>
2021-02-13 05:31:18 +0200

    Single Product Page

--- src/app/components/customers/single-product/single-product.component.ts
+++ src/app/components/customers/single-product/single-product.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { SubpagesService } from 'src/app/shared/subpages.service';
+import { PageTitle } from '../design-parts/titlePage.enum';
 
 @Component({
   selector: 'app-single-product',
   templateUrl: './single-product.component.html',
-  styleUrls: ['./single-product.component.css']
+  styleUrls: ['./single-product.component.css'],
 })
 export class SingleProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private subPageTitle: SubpagesService) {
+    this.subPageTitle.subPageTitle = PageTitle.singleProduct;
   }
 
+  ngOnInit(): void {}
 }
